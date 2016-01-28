f3967cb47ce012d34c3d6f8bf901aeecb4cddea6
yyfjmb <yyfjmb@hotmail.com>
2016-01-28 16:52:16 +0800

    add taiwan version

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,63 @@
+
+/*add taiwan transition*/
+
+var cloth1Ratio = 4.2
+var cloth2Ratio = -2.1
+var food1Ratio = 0.013*30
+var food2Ratio = 0.00045*30
+var house1Ratio = 0.57
+var house2Ratio = 0.66
+var house3Ratio = 1.75
+var house4Ratio = 1.88
+var transport1Ratio = 0.0114*30
+var transport2Ratio = 0.0493*30
+var transport3Ratio = 0.0940*30
+var transport4Ratio = 0.0414*30
+var transport5Ratio = 0.2119*30
+var transport6Ratio = 0
+
+$('#taiwan').click(function() {
+    if ($(this).text() === "轉台灣版") {
+        $('#versionControl1').text("天然氣")
+        $('#versionControl2').text("石油氣")
+        $('#versionControl3').text("捷運")
+        $('#versionControl4').text("公車")
+        $('#versionControl5').text("轎車")
+        $('#versionControl6').text("貨車")
+        $('#versionControl7').text("機車")
+        $('#versionControl8').text("腳踏車")
+        $('#versionControl9').text("km")
+        $('#versionControl10').text("km")
+        $('#versionControl11').text("km")
+        $('#versionControl12').text("km")
+        $(this).text("轉香港版")
+        var house1Ratio = 0.16
+        var house2Ratio = 0.52
+    } else {
+        $('#versionControl1').text("煤氣")
+        $('#versionControl2').text("瓦斯")
+        $('#versionControl3').text("MTR")
+        $('#versionControl4').text("巴士")
+        $('#versionControl5').text("小巴")
+        $('#versionControl6').text("的士")
+        $('#versionControl7').text("私家車")
+        $('#versionControl8').text("單車")
+        $('#versionControl9').text("元")
+        $('#versionControl10').text("元")
+        $('#versionControl11').text("元")
+        $('#versionControl12').text("元")
+        $(this).text("轉台灣版")
+        var transport1Ratio = 0.033*30
+        var transport2Ratio = 0.04*30
+        var transport3Ratio = 0.14*30
+        var transport4Ratio = 0.21*30
+        var transport5Ratio = 0.05*30
+    }
+})
+
+
+
+
 /*round slider*/
 
 $("#cloth1").roundSlider({
@@ -111,97 +171,30 @@ var transport4 = $("#transport4")
 var transport5 = $("#transport5")
 var transport6 = $("#transport6")
 
-// var coefficient = 0.5
 
-// $(".dropdown-menu a").click(function() {
-//     var transportId = "mtr"
-//     var nameMapping = {
-//         "mtr": "MTR",
-//         "taxi": "的士",
-//         "bus": "巴士",
-//         "minibus": "小巴",
-//         "car": "私家車",
-//         "bike": "單車"
-//     }
-//     var noteMapping = {
-//         "mtr": "所花費用",
-//         "taxi": "所花費用",
-//         "bus": "所花費用",
-//         "minibus": "所花費用",
-//         "car": "行駛距離",
-//         "bike": "行駛距離"
-//     }
-//     $(".dropdown-menu a").each(function(idx, elem) {
-//         $(elem).removeClass('selected')
-//     })
-//     $(this).addClass('selected')
-//     transportId = $(this).attr("id")
-//     $(":button").html(nameMapping[transportId] + " <span class='caret'></span>")
-//     $("#transportnote").text(noteMapping[transportId])
-// })
 
-// $("#mtr").click(function(){
-//     $("#transport").roundSlider({
-//         value: 3,
-//         max: 50
-//     })
-//     coefficient = 0.0114*30
-// })
-
-// $("#taxi").click(function(){
-//     $("#transport").roundSlider({
-//         value: 25,
-//         max: 200
-//     })
-//     coefficient = 0.0414*30
-// })
-
-// $("#bus").click(function(){
-//     $("#transport").roundSlider({
-//         value: 5,
-//         max: 50
-//     })
-//     coefficient = 0.0493*30
-// })
-
-// $("#car").click(function(){
-//     $("#transport").roundSlider({
-//         value: 2,
-//         max: 40,
-//     })
-//     coefficient = 0.2119*30
-// })
-
-// $("#minibus").click(function(){
-//     $("#transport").roundSlider({
-//         value: 5,
-//         max: 50,
-//     })
-//     coefficient = 0.094*30
-// })
-
-// $("#bike").click(function(){
-//     $("#transport").roundSlider({
-//         value: 60,
-//         max: 600
-//     })
-//     coefficient = 0
-// })
+/*calculate carbon number and tree number*/
 
 var treeNumber = 0
+var totalCarbon = 0
 
 function slideChange () {
-    var total= cloth1.getValue()*4.2/30 - cloth2.getValue()*2.1/30 + food1.getValue()*0.013 + food2.getValue()*0.45*0.001 + parseFloat(house1.attr( "value" ))*0.57 + parseFloat(house2.attr( "value" ))*0.66/30 + parseFloat(house3.attr( "value" ))*1.75/30 + parseFloat(house4.attr( "value" ))*1.88/30 + parseFloat(transport1.attr( "value" ))*0.0114/30 + parseFloat(transport2.attr( "value" ))*0.0493 + parseFloat(transport3.attr( "value" ))*0.0940 + parseFloat(transport4.attr( "value" ))*0.0414 + parseFloat(transport5.attr( "value" ))*0.2119
-    console.log(total.toFixed(2))
-    $( "#totalNumber" ).text(total.toFixed(2))
+    totalCarbon= cloth1.getValue()*cloth1Ratio + cloth2.getValue()*cloth2Ratio + food1.getValue()*food1Ratio + food2.getValue()*food2Ratio + parseFloat(house1.attr( "value" ))*house1Ratio + parseFloat(house2.attr( "value" ))*house2Ratio + parseFloat(house3.attr( "value" ))*house3Ratio + parseFloat(house4.attr( "value" ))*house4Ratio + parseFloat(transport1.attr( "value" ))*transport1Ratio + parseFloat(transport2.attr( "value" ))*transport2Ratio + parseFloat(transport3.attr( "value" ))*transport3Ratio + parseFloat(transport4.attr( "value" ))*transport4Ratio + parseFloat(transport5.attr( "value" ))*transport5Ratio
+    console.log(totalCarbon.toFixed(2))
 
-    treeNumber = (total/23).toFixed(2)
+    // $( "#totalCarbon" ).text(totalCarbon.toFixed(2))
+    var $newHead = $("<span> 您本月的碳排放量为 <span id='totalCarbon'>" + totalCarbon.toFixed(2) + "</span> kg CO2e </span>")
+    $('.screen .jumbotron #announce').html($newHead)
+
+    treeNumber = (totalCarbon/19.16).toFixed(2)
 }
 
 var verticalChange = $('.verticleSlider').on('slide', function(){
-    var total= cloth1.getValue()*4.2/30 - cloth2.getValue()*2.1/30 + food1.getValue()*0.013 + food2.getValue()*0.45*0.001 + parseFloat(house1.attr( "value" ))*0.57 + parseFloat(house2.attr( "value" ))*0.66/30 + parseFloat(house3.attr( "value" ))*1.75/30 + parseFloat(house4.attr( "value" ))*1.88/30 + parseFloat(transport1.attr( "value" ))*0.0114/30 + parseFloat(transport2.attr( "value" ))*0.0493 + parseFloat(transport3.attr( "value" ))*0.0940 + parseFloat(transport4.attr( "value" ))*0.0414 + parseFloat(transport5.attr( "value" ))*0.2119
-    console.log(total.toFixed(2))
-    $( "#totalNumber" ).text(total.toFixed(2))
+    totalCarbon= cloth1.getValue()*cloth1Ratio + cloth2.getValue()*cloth2Ratio + food1.getValue()*food1Ratio + food2.getValue()*food2Ratio + parseFloat(house1.attr( "value" ))*house1Ratio + parseFloat(house2.attr( "value" ))*house2Ratio + parseFloat(house3.attr( "value" ))*house3Ratio + parseFloat(house4.attr( "value" ))*house4Ratio + parseFloat(transport1.attr( "value" ))*transport1Ratio + parseFloat(transport2.attr( "value" ))*transport2Ratio + parseFloat(transport3.attr( "value" ))*transport3Ratio + parseFloat(transport4.attr( "value" ))*transport4Ratio + parseFloat(transport5.attr( "value" ))*transport5Ratio
+    var $newHead = $("<span> 您本月的碳排放量为 <span id='totalCarbon'>" + totalCarbon.toFixed(2) + "</span> kg CO2e </span>")
+    $('.screen .jumbotron #announce').html($newHead)
+
+    $( "#totalCarbon" ).text(totalCarbon.toFixed(2))
     $("#house1Value" ).text( $("#house1" ).attr( "value" ))
     $("#house2Value" ).text( $("#house2" ).attr( "value" ))
     $("#house3Value" ).text( $("#house3" ).attr( "value" ))
@@ -213,39 +206,38 @@ var verticalChange = $('.verticleSlider').on('slide', function(){
     $("#transport5Value" ).text( $("#transport5" ).attr( "value" ))
     $("#transport6Value" ).text( $("#transport6" ).attr( "value" ))
 
-     treeNumber = (total/23).toFixed(2)
+    treeNumber = (totalCarbon/19.16).toFixed(2)
 })
+
+
+
+/*add result page*/
+
 
 $('#lightboxTrigger').click(function() {
     $('#lightbox').fadeIn(500);
 
     console.log(treeNumber)
 
-
     var $newimg
-    if (treeNumber > 19) {
-        treeNumber = 18
-    }
-    for(var i=0;i<parseInt(treeNumber)+1;i++) {
-        $newimg = $('<img class="tree" src="images/tree.svg" />')
+    for(var i=0;i<(parseInt(treeNumber)+1);i++) {
+        $newimg = $('<div class="tree" />')
         $('#treeArea').append($newimg)
     }
-    $('#treeArea img:last-child').css("opacity", (treeNumber-parseInt(treeNumber)))
+    $('#treeArea div:last-child').css("opacity", (treeNumber-parseInt(treeNumber)))
     $('#treeNumber').text(treeNumber)
+    if ((treeNumber > 18) && (treeNumber < 38)) {
+        $('#treeArea div').addClass("narrow").removeClass("xnarrow")
+    } else if (treeNumber >= 38){
+        $('#treeArea div').removeClass("narrow").addClass("xnarrow")
+    }
+    $( "#totalCarbon2" ).text(totalCarbon.toFixed(2))
+    $('.container').hide()
+    // $("meta[property='og\\:description']").attr("content", treeNumber);
 })
 
 $('#close').click(function() {
     $('#lightbox').fadeOut(500)
-    $('#treeArea img').remove()
+    $('.container').show()
+    $('#treeArea div').remove()
 })
-
-// $(".dropdown-menu").on('click', 'li a', function(){
-//       $(".btn:first-child").text($(this).text());
-//       $(".btn:first-child").val($(this).text());
-//    });
-
-// $(".verticleSlider").slider({
-// 	formatter: function(value) {
-// 		return 'Current value: ' + value;
-// 	}
-// });
